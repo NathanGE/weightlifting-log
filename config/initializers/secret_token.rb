e6b23d189af20c09f83d0df527e094242c388aff
Nathan <nathangelliott@gmail.com>
2015-12-28 02:21:13 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WeightliftingLog::Application.config.secret_key_base = 'ab1f50ff546a12a49b403f3f41cac12d536747588131a4eb2ad142c97dae219119d330d1367cbee89bc60d2685e82a0461492df97be9bcb218936e13ec7ff979'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WeightliftingLog::Application.config.session_store :cookie_store, key: '_weightlifting-log_session'
