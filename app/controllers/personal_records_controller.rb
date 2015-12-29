c5f3f4cf20779ec1217849ef51e198c7303e33e8
Nathan <nathangelliott@gmail.com>
2015-12-29 01:29:36 +0000

    Added functionality to the new personal record  form submit button

--- app/controllers/personal_records_controller.rb
+++ app/controllers/personal_records_controller.rb
@@ -7,4 +7,16 @@ class PersonalRecordsController < ApplicationController
   def new
     @personal_record = PersonalRecord.new
   end
+
+  def create
+    PersonalRecord.create(personal_record_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def personal_record_params
+    params.require(:personal_record).permit(:exercise, :weight, :date)
+  end
+
 end
